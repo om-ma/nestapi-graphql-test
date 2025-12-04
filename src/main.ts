aa923dd5aa04d981babd9f3e36f497829c3fdb1b
om-ma <omairrr.azam@gmail.com>
2025-12-05 01:46:50 +0500

    cors error resolution

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://18.139.37.51:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
